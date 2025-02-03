756fa92a6634712a4abd9ed2fa8ab66f2e2e5136
Henri Karkkainen <karkhkxy@exam-agc3313-5.it.jyu.fi>
2025-02-03 15:09:55 +0200

    Settings mockup

---
+++ src/main/java/com/actions/Settings.java
@@ -0,0 +1,34 @@
+package com.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class Settings extends AnAction {
+    private SettingsScreen window = null;
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        this.window = new SettingsScreen();
+        showSettings();
+    }
+
+    private void showSettings() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame newWindow = new JFrame("Settings");
+                newWindow.add(window.getContent());
+                newWindow.setSize(400, 300);
+                newWindow.setVisible(true);
+            }
+        });
+    }
+
+    public void displaySettings() {
+        this.window = new SettingsScreen();
+        showSettings();
+    }
+}
