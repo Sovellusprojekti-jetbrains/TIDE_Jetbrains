aeec808211091396aaac2b30b38e4cf6b1dda022
Henri <karkhkxy@student.jyu.fi>
2025-02-07 16:02:51 +0200

    Polku saadaan asetettua, mutta ei säily uudelleen käynnistämisien välissä

--- src/main/java/com/actions/Settings.java
+++ src/main/java/com/actions/Settings.java
@@ -2,8 +2,14 @@ package com.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
+import com.intellij.util.xmlb.XmlSerializerUtil;
 import com.views.SettingsScreen;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
@@ -12,10 +18,15 @@ import java.awt.event.WindowEvent;
 /**
  * This class is used to display settings window to configure tide-settings
  */
-public class Settings extends AnAction {
+@State(
+        name = "Settings",
+        storages = @Storage(StoragePathMacros.WORKSPACE_FILE)
+)
+public class Settings extends AnAction implements PersistentStateComponent<com.actions.Settings.State> {
     private SettingsScreen window = null; //Object reference to window content
     private static boolean visible = false; //To ensure that only one settings window can be open at a time
     private static JFrame frame = null; //Object reference to JFrame containing window content
+    private static final State settings = new State(); //To save settings
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
@@ -52,5 +63,36 @@ public class Settings extends AnAction {
     public static void close() {
         frame.dispose();
         visible = false;
+    }
+
+    public static class State {
+        public String path = null;
+        public final String DEFAULT_PATH = System.getProperty("user.dir");
+    }
+
+    @Override
+    public @Nullable Settings.State getState() {
+        return settings;
+    }
+
+    @Override
+    public void loadState(@NotNull Settings.State state) {
+        XmlSerializerUtil.copyBean(state, settings);
+    }
+
+    @Override
+    public void initializeComponent() {
+        PersistentStateComponent.super.initializeComponent();
+    }
+
+    public static String getPath() {
+        if (settings.path == null) {
+            return settings.DEFAULT_PATH;
+        }
+        return settings.path;
+    }
+
+    public static void savePath(String path) {
+        settings.path = path;
     }
 }
--- src/main/java/com/views/SettingsScreen.java
+++ src/main/java/com/views/SettingsScreen.java
@@ -3,23 +3,37 @@ package com.views;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * Content to be displayed in settings window
  */
 public class SettingsScreen {
     private JPanel settings;
-    private JTextField cDevelOhj1TextField;
+    private JTextField pathText;
     private JButton browseButton;
     private JButton OKButton;
     private JButton cancelButton;
     private JPanel path;
     private JPanel buttons;
 
-
+    /**
+     * Constructor to add action listeners for buttons
+     */
     public SettingsScreen() {
-        //TODO: Action listener for ok button to save changes and to close the settings window
-        //TODO: Action listener for browse button to call OS file explorer to choose and set file path
+        this.pathText.setText(com.actions.Settings.getPath());
+        this.browseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                choosePath();
+            }
+        });
+        this.OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updatePath();
+            }
+        });
         this.cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -28,6 +42,30 @@ public class SettingsScreen {
         });
     }
 
+    /**
+     * Displays a new window where user can choose folder to which demos will be saved
+     */
+    private void choosePath() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        JFrame frame = new JFrame("Choose folder");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        int value = chooser.showOpenDialog(frame);
+        if (value == JFileChooser.APPROVE_OPTION) {
+            File selectedFolder = chooser.getSelectedFile();
+            this.pathText.setText(selectedFolder.getAbsolutePath());
+        }
+    }
+
+    private void updatePath() {
+        com.actions.Settings.savePath(this.pathText.getText());
+        com.actions.Settings.close();
+    }
+
+    /**
+     * Return main panel to be displayed
+     * @return JPanel containing all the elements
+     */
     public JPanel getContent() {
         return settings;
     }
