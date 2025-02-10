dee62f27f58511fae73bd1d629f8e302270c0f83
Lari Salminen <lari.j.a.salminen@student.jyu.fi>
2025-02-10 13:29:59 +0200

    Additional fixes

--- src/main/java/com/views/CustomScreen.java
+++ src/main/java/com/views/CustomScreen.java
@@ -16,17 +16,44 @@ import java.io.InputStreamReader;
  * Class for displaying a template course window.
  */
 public class CustomScreen {
+    /**
+     * The button that does the login action.
+     */
     private JButton loginButton;
+    /**
+     * The main pane for the window.
+     */
     private JPanel panel1;
+    /**
+     * The pane that contains all the tabs for the window.
+     */
     private JTabbedPane tabbedPane;
+    /**
+     * The pane containing the login and settings buttons.
+     */
     private JPanel loginPane;
-    private JPanel titlePanel;
-    private JLabel courseLabel;
+    // private JPanel titlePanel;
+    // private JLabel courseLabel;
+    /**
+     * Course panel contains all the courses and their exercises.
+     */
     private JPanel coursePanel;
+    /**
+     * Courses pane should be turned into a normal pane instead of a scrollPane.
+     */
     private JScrollPane coursesPane;
+    /**
+     * Button for logging out.
+     */
     private JButton logoutButton;
+    /**
+     * Button that opens the settings window.
+     */
     private JButton settingsButton;
-    Color bgColor = new Color(40,40,40);
+    private final int red = 40;
+    private final int green = 40;
+    private final int blue = 40;
+    private final Color bgColor = new Color(red, green, blue);
 
     /**
      * Constructor for CustomScreen.
@@ -73,13 +100,13 @@ public class CustomScreen {
             panel.setBackground(bgColor);
             panel.setOpaque(true);
 
-            final int PANESIZE = 300;
-            final int THICKNESS = 4;
+            final int paneSize = 300;
+            final int thickness = 4;
 
             // Tehdään scrollpane johon lätkäistään kaikki tähän mennessä tehty.
             JScrollPane scrollPane = new JBScrollPane(panel);
-            scrollPane.setPreferredSize(new Dimension(PANESIZE, PANESIZE)); // Set limited height
-            scrollPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, THICKNESS));
+            scrollPane.setPreferredSize(new Dimension(paneSize, paneSize)); // Set limited height
+            scrollPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, thickness));
             scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
             scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
@@ -92,11 +119,8 @@ public class CustomScreen {
         panel1.repaint();
         switchToLogin();
 
-
-        /**
-         * currently assumes that the user has the TIM CLI installed
-         * need some checks and tests in the future.
-         */
+         // currently assumes that the user has the TIM CLI installed
+         // need some checks and tests in the future.
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -122,9 +146,7 @@ public class CustomScreen {
             }
         });
 
-        /**
-         * Adds an action listener for the settings button.
-         */
+        // Adds an action listener for the settings button
         settingsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
@@ -133,9 +155,8 @@ public class CustomScreen {
             }
         });
 
-        /**
-         * Adds an action listener for the logout button.
-         */
+
+        // Adds an action listener for the logout button.
         logoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
@@ -164,6 +185,10 @@ public class CustomScreen {
 
     }
 
+    /**
+     * Gets the content of the panel.
+     * @return main panel
+     */
     public JPanel getContent() {
         return panel1;
     }
@@ -174,12 +199,12 @@ public class CustomScreen {
      * @return Viikkotehtävärivi
      */
     JPanel createExercise(int name) {
-        final int FONTSIZE = 16;
+        final int fontSize = 16;
         JPanel subPanel = new JPanel();
         subPanel.setLayout(new BorderLayout());
         JLabel labelWeek = new JLabel();
         labelWeek.setText("Label " + name);
-        labelWeek.setFont(new Font("Arial", Font.BOLD, FONTSIZE));
+        labelWeek.setFont(new Font("Arial", Font.BOLD, fontSize));
         subPanel.add(labelWeek, BorderLayout.WEST);
 
         JPanel buttonPanel = new JPanel(new FlowLayout());
