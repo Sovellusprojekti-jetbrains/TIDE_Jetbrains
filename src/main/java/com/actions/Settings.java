585b93d2c3bd537ff71002d900a20f2ba9ce1c1d
Henri <karkhkxy@student.jyu.fi>
2025-02-10 12:13:50 +0200

    Persistent state component alustava toteutus

--- src/main/java/com/actions/Settings.java
+++ src/main/java/com/actions/Settings.java
@@ -2,14 +2,9 @@ package com.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
-import com.intellij.openapi.components.StoragePathMacros;
-import com.intellij.util.xmlb.XmlSerializerUtil;
+import com.intellij.openapi.application.ApplicationManager;
 import com.views.SettingsScreen;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
@@ -18,22 +13,24 @@ import java.awt.event.WindowEvent;
 /**
  * This class is used to display settings window to configure tide-settings
  */
-@State(
-        name = "Settings",
-        storages = @Storage(StoragePathMacros.WORKSPACE_FILE)
-)
-public class Settings extends AnAction implements PersistentStateComponent<com.actions.Settings.State> {
+public class Settings extends AnAction {
     private SettingsScreen window = null; //Object reference to window content
     private static boolean visible = false; //To ensure that only one settings window can be open at a time
     private static JFrame frame = null; //Object reference to JFrame containing window content
-    private static final State settings = new State(); //To save settings
 
+    /**
+     * If settings is called via an action, this method is called
+     * @param e Action event (not used)
+     */
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         this.window = new SettingsScreen();
         showSettings();
     }
 
+    /**
+     * Displays settings window if one is not already open
+     */
     private void showSettings() {
         if (!visible) {
             visible = true;
@@ -55,44 +52,35 @@ public class Settings extends AnAction implements PersistentStateComponent<com.a
         }
     }
 
+    /**
+     * This method can be called from action listeners to open settings window
+     */
     public void displaySettings() {
         this.window = new SettingsScreen();
         showSettings();
     }
 
+    /**
+     * Closes settings window
+     */
     public static void close() {
         frame.dispose();
         visible = false;
     }
 
-    public static class State {
-        public String path = null;
-        public final String DEFAULT_PATH = System.getProperty("user.dir");
-    }
-
-    @Override
-    public @Nullable Settings.State getState() {
-        return settings;
-    }
-
-    @Override
-    public void loadState(@NotNull Settings.State state) {
-        XmlSerializerUtil.copyBean(state, settings);
-    }
-
-    @Override
-    public void initializeComponent() {
-        PersistentStateComponent.super.initializeComponent();
-    }
-
+    /**
+     * Calls StateManager for getting user defined file path
+     * @return File path as a String
+     */
     public static String getPath() {
-        if (settings.path == null) {
-            return settings.DEFAULT_PATH;
-        }
-        return settings.path;
+       return ApplicationManager.getApplication().getService(StateManager.class).getPath();
     }
 
+    /**
+     * Calls StateManager for setting new path defined by user
+     * @param path File path as a String
+     */
     public static void savePath(String path) {
-        settings.path = path;
+        ApplicationManager.getApplication().getService(StateManager.class).setPath(path);
     }
 }
---
+++ src/main/java/com/actions/StateManager.java
@@ -0,0 +1,63 @@
+package com.actions;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.*;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This class implements persistent state component and provides a service for getting and setting user defined plugin settings
+ */
+@Service
+@State(
+        name = "StateManager",
+        storages = @Storage("$APP_CONFIG$/mySettings.xml")
+)
+public final class StateManager implements PersistentStateComponent<StateManager.State> {
+
+    /**
+     * Separate state class to hold the user defined settings.
+     */
+    static class State {
+        public String path;
+    }
+
+    private State myState = new State(); //Object reference to state class
+
+    /**
+     * This method is called when updating state class fields and to save the state of the State class when IDE is closed
+     * @return Object reference to State class
+     */
+    @Override
+    public State getState() {
+        return myState;
+    }
+
+    /**
+     * This method is called when IDE is opened to load previous component state
+     * @param state loaded component state
+     */
+    @Override
+    public void loadState(@NotNull State state) {
+        System.out.println("Settings loaded");
+        myState = state;
+    }
+
+    /**
+     * Sets new value to path field in State class
+     * @param path File path as a String
+     */
+    public void setPath(String path) {
+        getState().path = path;
+    }
+
+    /**
+     * Gets the path fields value from State class
+     * @return File ath as a String
+     */
+    public String getPath() {
+        if (getState().path == null) {
+            return System.getProperty("user.dir");
+        }
+        return getState().path;
+    }
+}
