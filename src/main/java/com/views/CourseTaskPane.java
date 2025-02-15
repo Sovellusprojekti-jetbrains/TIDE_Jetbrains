af3c07b866dae9527861c49cbec41af4844d869e
tjmeril <tero.j.merilainen@student.jyu.fi>
2025-02-16 00:13:39 +0200

    Get current exercise path and add placeholder functionality to coursepane

--- src/main/java/com/views/CourseTaskPane.java
+++ src/main/java/com/views/CourseTaskPane.java
@@ -3,7 +3,16 @@
 
 package com.views;
 
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
 
 /**
 * Hoidetaan kaikki ruudun oikealla puolella olevan tehtävän palautuksen suorittavan ikkunan toiminnalliset sekä graaffiset toiminnot
@@ -69,6 +78,10 @@ public class CourseTaskPane {
      * Reset panel.
      */
     private JPanel resetPane;
+    /**
+     * Holds the current project.
+     */
+    private Project project;
 
     /**
      * getter for the contents of the task panel.
@@ -76,6 +89,45 @@ public class CourseTaskPane {
      */
     public JPanel getContent() {
         return taskPane;
+    }
+
+
+    /**
+     * A constructor that takes a ToolWindow as a parameter.
+     * The Toolwindow instance lets us access the current project
+     * and thus the path of the currently open file.
+     * @param toolWindow A ToolWindow instance
+     */
+    public CourseTaskPane(final ToolWindow toolWindow) {
+        this.project = toolWindow.getProject();
+
+        // placeholder for opening the current exercise in browser
+        avaaTehtava.addActionListener(event -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://tim.jyu.fi"));
+            } catch (IOException | URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        // placeholder for resetting exercises;
+        // should behave similar to com.actions.Reset, so both
+        // can delegate processing the path to a third party
+        resetButton.addActionListener(event -> {
+            String path = Objects.requireNonNull(FileEditorManager
+                    .getInstance(project)
+                    .getSelectedEditor())
+                    .getFile()
+                    .getPath();
+            System.out.println(path);
+        });
+
+        // placeholder for submitting exercises
+        submitButton.addActionListener(event -> {
+            String path = FileEditorManager.getInstance(project).getSelectedEditor().getFile().getPath();
+            // kutsu tehtävänlataajaa vivulla -f
+            System.out.println(path);
+        });
     }
 }
 
