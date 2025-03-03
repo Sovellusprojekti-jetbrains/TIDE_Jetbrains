45c0918583ca971ad5133a66ebfdc421fe1e7de6
Spanzer0 <124801765+Spanzer0@users.noreply.github.com>
2025-03-03 14:59:06 +0200

    added automatic creation of new project

--- src/test/java/UiTest.java
+++ src/test/java/UiTest.java
@@ -1,8 +1,13 @@
 import com.intellij.remoterobot.RemoteRobot;
-import com.intellij.remoterobot.fixtures.ComponentFixture;
+import com.intellij.remoterobot.data.RemoteComponent;
+import com.intellij.remoterobot.fixtures.*;
 import com.intellij.remoterobot.search.locators.Locator;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import static com.intellij.remoterobot.search.locators.Locators.byXpath;
+import static java.time.Duration.ofMinutes;
+import static java.time.Duration.ofSeconds;
+import static org.apache.commons.io.FileUtils.waitFor;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -12,31 +17,98 @@ public class UiTest {
     //opens the remote robot instance into localhost at port 8082.
     //you can check xpath of the components by putting the url into a browser window after running ./gradlew clean runIdeForUiTests.
    private final RemoteRobot remoteRobot = new RemoteRobot("http://127.0.0.1:8082");
-    // Remote robot locators that are needed to find the components from the xpath file.
-    private final Locator tideLocator = byXpath("//div[@class='TabPanel'][.//div[@text='Courses']]");
-    private final Locator courseTaskLocator = byXpath("//div[@class='TabPanel'][.//div[@text='Course Task']]");
-    private final Locator loginLocator = byXpath("//div[@text='Login']");
-    private final Locator settingsLocator = byXpath("//div[@text='Settings']");
-    private final Locator openButtonLocator = byXpath("//div[@text='Open exercise']");
-    private final Locator submitButtonLocator = byXpath("//div[@text='Submit']");
-    private final Locator outputButtonLocator = byXpath("//div[@text='Show output']");
-    private final  Locator resetButtonLocator = byXpath("//div[@text='Reset exercise']");
-    //The components that are used for the tests.
-    private final ComponentFixture tide = remoteRobot.find(ComponentFixture.class, tideLocator);
-    private final ComponentFixture courses = remoteRobot.find(ComponentFixture.class, courseTaskLocator);
-    private final ComponentFixture login = remoteRobot.find(ComponentFixture.class, loginLocator);
-    private final ComponentFixture settings = remoteRobot.find(ComponentFixture.class, settingsLocator);
-    private final ComponentFixture openButton = remoteRobot.find(ComponentFixture.class, openButtonLocator);
-    private final ComponentFixture submitButton = remoteRobot.find(ComponentFixture.class, submitButtonLocator);
-    private final ComponentFixture outputButton = remoteRobot.find(ComponentFixture.class, outputButtonLocator);
-    private final ComponentFixture resetButton = remoteRobot.find(ComponentFixture.class, resetButtonLocator);
 
+    @DefaultXpath(by = "FlatWelcomeFrame type", xpath = "//div[@class='FlatWelcomeFrame']")
+    @FixtureName(name = "Welcome Frame")
+   public static class WelcomeFrameFixture extends ContainerFixture {
+        /**
+         * Self-made fixture class for the welcome frame.
+         * @param robot the remoterobot
+         * @param remoteComponent the remoteComponent
+         */
+        public WelcomeFrameFixture(@NotNull RemoteRobot robot, @NotNull RemoteComponent remoteComponent) {
+            super(robot, remoteComponent);
+        }
 
+        // Create New Project
+
+        /**
+         * returns the create new project button.
+         * @return the button to create a new project.
+         */
+        public ComponentFixture createNewProjectLink() {
+            return find(ComponentFixture.class, byXpath("//div[@defaulticon='createNewProjectTab.svg']"));
+        }
+    }
+
+    @FixtureName(name = "Idea frame")
+    @DefaultXpath(by = "IdeFrameImpl type", xpath = "//div[@class='IdeFrameImpl']")
+    public static class IdeaFrame extends CommonContainerFixture {
+        /**
+         * Self-made fixture class for the basic ide frame.
+         * @param robot the remoterobot
+         * @param remoteComponent the remoteComponent
+         */
+        public IdeaFrame(@NotNull RemoteRobot robot, @NotNull RemoteComponent remoteComponent) {
+            super(robot, remoteComponent);
+        }
+
+        /**
+         * checks if ide is in dumb mode
+         * @return true if ide is in dumb mode, false if not.
+         */
+        public boolean isDumbMode() {
+            return callJs(
+                    """
+                    const frameHelper = com.intellij.openapi.wm.impl.ProjectFrameHelper.getFrameHelper(component)
+                    if (frameHelper) {
+                        const project = frameHelper.getProject()
+                        project ? com.intellij.openapi.project.DumbService.isDumb(project) : true
+                    } else { true }
+                    """, true);
+        }
+    }
+    
     /**
      * Tests the text contents of the tide and Courses task tool windows and the buttons.
      */
     @Test
-    public void textTest() {
+    public void textTest() throws InterruptedException {
+        WelcomeFrameFixture welcomeFrame = remoteRobot.find(WelcomeFrameFixture.class);
+        welcomeFrame.createNewProjectLink().click();
+        final Locator createButton = byXpath("//div[@text='Create']");
+        final ComponentFixture createFixture = remoteRobot.find(ComponentFixture.class, createButton);
+        createFixture.click();
+        final IdeaFrame idea = remoteRobot.find(IdeaFrame.class, ofSeconds(10));
+        waitFor(ofMinutes(2), () -> !idea.isDumbMode());
+        final Locator tideButtonLocator = byXpath("//div[@tooltiptext='TIDE Tool Window']");
+        final Locator courseButtonLocator = byXpath("//div[@tooltiptext='Course Task']");
+        final ComponentFixture tideButton = remoteRobot.find(ComponentFixture.class, tideButtonLocator);
+        final ComponentFixture courseButton = remoteRobot.find(ComponentFixture.class, courseButtonLocator);
+
+        tideButton.click();
+        courseButton.click();
+        // Remote robot locators that are needed to find the components from the xpath file.
+
+
+        final Locator tideLocator = byXpath("//div[@class='TabPanel'][.//div[@text='Courses']]");
+        final Locator courseTaskLocator = byXpath("//div[@class='TabPanel'][.//div[@text='Course Task']]");
+        final Locator loginLocator = byXpath("//div[@text='Login']");
+        final Locator settingsLocator = byXpath("//div[@text='Settings']");
+        final Locator openButtonLocator = byXpath("//div[@text='Open exercise']");
+        final Locator submitButtonLocator = byXpath("//div[@text='Submit']");
+        final Locator outputButtonLocator = byXpath("//div[@text='Show output']");
+        final  Locator resetButtonLocator = byXpath("//div[@text='Reset exercise']");
+        //The components that are used for the tests.
+        final ComponentFixture tide = remoteRobot.find(ComponentFixture.class, tideLocator);
+        final ComponentFixture courses = remoteRobot.find(ComponentFixture.class, courseTaskLocator);
+        final ComponentFixture login = remoteRobot.find(ComponentFixture.class, loginLocator);
+        final ComponentFixture settings = remoteRobot.find(ComponentFixture.class, settingsLocator);
+        final ComponentFixture openButton = remoteRobot.find(ComponentFixture.class, openButtonLocator);
+        final ComponentFixture submitButton = remoteRobot.find(ComponentFixture.class, submitButtonLocator);
+        final ComponentFixture outputButton = remoteRobot.find(ComponentFixture.class, outputButtonLocator);
+        final ComponentFixture resetButton = remoteRobot.find(ComponentFixture.class, resetButtonLocator);
+
         assertTrue(tide.hasText("TIDE Tool Window"), "TIDE Tool Window has wrong text or doesn't render");
         assertTrue(tide.hasText("Courses"), "TIDE Tool Windows Courses tab has wrong text or doesn't render");
         assertTrue(login.hasText("Login"), "Login button has wrong text or doesn't render");
@@ -60,4 +132,7 @@ public class UiTest {
 //div[@text='Open exercise']
 //div[@text='Submit']
 //div[@text='reset exercise']
+
+//div[@tooltiptext='TIDE Tool Window']
+//div[@tooltiptext='Course Task']
 
