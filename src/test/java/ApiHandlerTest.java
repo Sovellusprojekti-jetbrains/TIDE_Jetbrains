741d51470af355e6b47ee8cf7cb7683d6fbd97d3
Lari Salminen <lari.j.a.salminen@student.jyu.fi>
2025-04-04 12:17:41 +0300

    Removed unused imports.

--- src/test/java/ApiHandlerTest.java
+++ src/test/java/ApiHandlerTest.java
@@ -1,5 +1,3 @@
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 import com.api.ApiHandler;
@@ -8,8 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.List;
 
 public class ApiHandlerTest {
     private ApiHandler apiHandler;
