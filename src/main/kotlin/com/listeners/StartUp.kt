7bedd03f39e76aed2a8f2a5b519f1a5b2d08ff56
saseilmo <sasu.ilmo@gmail.com>
2025-04-04 14:24:13 +0300

    Forces tide toolwindow to open in starup

--- src/main/kotlin/com/listeners/StartUp.kt
+++ src/main/kotlin/com/listeners/StartUp.kt
@@ -1,11 +1,10 @@
 package com.listeners
 
-import com.actions.ActiveState
-import com.api.TideCommandExecutor
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.wm.ToolWindowManager
 
 class StartUp : StartupActivity {
 
@@ -13,7 +12,7 @@ class StartUp : StartupActivity {
       override fun runActivity(project: Project) {
 
         ApplicationManager.getApplication().invokeLater {
-            val programName = "TIDE"
+            val programName = "tide"
             if (System.getProperty("os.name").contains("Windows")) {
 
 
@@ -39,13 +38,29 @@ class StartUp : StartupActivity {
                 }
             }
 
-
+            /*
             //tabbedPane.remove(loginPane); // Hide Login tab
+            val api = ApiHandler()
+            val result = async { api.checkLogin()}
             TideCommandExecutor.checkLogin()
+            System.out.println()
             val stateManager = ActiveState.getInstance()
             stateManager.updateCourses()
-
+            */
         }
+          showWindow(project)
+
+    }
+
+    /**
+     * Displays the toolwindow.
+     * @param project The current project
+     */
+    fun showWindow(project: Project) {
+
+            val toolWindowManager = ToolWindowManager.getInstance(project)
+            val window = checkNotNull(toolWindowManager.getToolWindow("TIDE Tool Window"))
+            window.show(null)
 
     }
 
