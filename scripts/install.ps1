0175d01f04c7e908c829ca215cdbb095c4e2b550
saseilmo <sasu.ilmo@gmail.com>
2025-04-07 15:42:21 +0300

    final version of the ps1 script and finished installscreen

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,10 +1,17 @@
 $userName = $env:USERNAME
 
-$directoryPath = "C:\" + $userName + "\tide"
+$directoryPath = "C:\users\" + $userName + "\tide"
 
 if(-not (Test-Path -Path $directoryPath -PathType Container)) {
     mkdir $directoryPath
 
-} else {
-
 }
+Invoke-WebRequest -Uri "https://github.com/TIDE-project/TIDE-CLI/releases/latest/download/tide-windows-latest.zip" -OutFile "tide.zip"
+
+Expand-Archive .\tide.zip -DestinationPath $directoryPath
+
+[Environment]::SetEnvironmentVariable("Path", $env:Path + $directoryPath,"Machine")
+
+Set-ItemProperty -Path "$directoryPath + "\tide.exe" -Name IsReadOnly $false
+
+
---
+++ src/main/java/com/views/InstallScreen.java
@@ -0,0 +1,94 @@
+package com.views;
+
+import com.intellij.openapi.ui.Messages;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+public class InstallScreen {
+    private JButton installAutomaticallyButton;
+    private JButton installManuallyButton;
+    private JPanel installPane;
+    private Boolean visible = false;
+    private static JFrame frame = null;
+
+
+    /**
+     * Handles the actions concerning the installation of the Tide-CLI.
+     */
+    public InstallScreen() {
+        installAutomaticallyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (System.getProperty("os.name").contains("Windows")) {
+                    //TODO: call install script.
+                    ProcessBuilder process = new ProcessBuilder("powershell.exe ./scripts/install.ps1");
+                    return;
+                }
+                if (System.getProperty("os.name").contains("Linux")) {
+                    //Automatic install not possible on Linux
+                    //Would require too many dependencies to be installed
+                    //technically possible if user has Python, Poetry and Pyenv
+                    Messages.showMessageDialog(
+                            "Automatic installation is not possible in Linux, please install Tide-CLI manually",
+                            "No Linux Support For Installation",
+                            Messages.getInformationIcon()
+                    );
+                    return;
+                }
+                if (System.getProperty("os.name").contains("Mac OS X")) {
+                    //TODO: test this on a mac to make sure it works
+
+                    try {
+                        ProcessBuilder process = new ProcessBuilder();
+                        process.command("curl -kLSs "
+                                + "https://github.com/TIDE-project/TIDE-CLI/releases/latest/download/tide-macos-latest.zip"
+                                + " -o tide.zip");
+                        process.start();
+                        process.command("unzip tide.zip");
+                        process.start();
+                        process.command("chmod +x tide");
+                        process.start();
+                        process.command("mv tide /usr/local/bin");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                    return;
+                }
+            }
+        });
+        installManuallyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                    try {
+                        Desktop.getDesktop().browse(
+                                new URI("https://tim.jyu.fi/view/kurssit/tie/proj/2024/tide/dokumentit/"
+                                        + "kayttoohjeet/tide-komentorivityokalun-asennusohjeet"));
+                    } catch (IOException | URISyntaxException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+            }
+        });
+
+
+
+    }
+
+    /**
+     * returns the Jpanel that is made.
+     * @return Jpanel that is made
+     */
+    public JPanel getContent() {
+        return this.installPane;
+    }
+}
