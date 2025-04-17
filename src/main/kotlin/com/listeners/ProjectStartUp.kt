5e175e7a474ae2effe690b56cc0e888513f0e0d2
Henri <karkhkxy@student.jyu.fi>
2025-04-17 11:09:54 +0300

    Starup odottaa että vasen ikkuna on valmis ennen kuin kurssit haetaan

--- src/main/kotlin/com/listeners/ProjectStartUp.kt
+++ src/main/kotlin/com/listeners/ProjectStartUp.kt
@@ -3,6 +3,7 @@ package com.listeners
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.api.TideCommandExecutor
+import com.intellij.openapi.wm.ToolWindowManager
 import kotlinx.coroutines.runBlocking
 
 
@@ -13,6 +14,14 @@ class ProjectStartUp : ProjectActivity {
         com.intellij.openapi.project.DumbService.getInstance(project).runWhenSmart { //Project must be ready first
             runBlocking { //Tries to prevent initialization of windows until login status is checked
                 TideCommandExecutor.checkLogin()
+            }
+            runBlocking { //Not ideal but maybe better solution will be found
+                while (true) {
+                    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("TIDE Tool Window")
+                    if (toolWindow != null && (toolWindow.isAvailable)) {
+                        break;
+                    }
+                }
             }
             TideCommandExecutor.fetchCoursesAsync()
         }
