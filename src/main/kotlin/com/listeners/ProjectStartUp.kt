db243c3ed1112b17db21cfa94bf5078833eed3e5
Henri <karkhkxy@student.jyu.fi>
2025-04-17 10:37:59 +0300

    Kirjautuminen tarkistetaan siten että alustus odottaa

--- src/main/kotlin/com/listeners/ProjectStartUp.kt
+++ src/main/kotlin/com/listeners/ProjectStartUp.kt
@@ -3,6 +3,7 @@ package com.listeners
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.api.TideCommandExecutor
+import kotlinx.coroutines.runBlocking
 
 
 class ProjectStartUp : ProjectActivity {
@@ -10,7 +11,9 @@ class ProjectStartUp : ProjectActivity {
 
     override suspend fun execute(project: Project) {
         com.intellij.openapi.project.DumbService.getInstance(project).runWhenSmart { //Project must be ready first
-            TideCommandExecutor.checkLogin()
+            runBlocking { //Tries to prevent initialization of windows until login status is checked
+                TideCommandExecutor.checkLogin()
+            }
             TideCommandExecutor.fetchCoursesAsync()
         }
     }
