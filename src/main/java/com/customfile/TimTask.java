3f3cfabae804a23d35ab7fe5e8cf15f7327072dd
Henri <karkhkxy@student.jyu.fi>
2025-04-24 15:05:02 +0300

    Parannuksia TimTaskiin

--- src/main/java/com/customfile/TimTask.java
+++ src/main/java/com/customfile/TimTask.java
@@ -23,12 +23,11 @@ import java.util.HashMap;
  */
 public final class TimTask {
 
-    private static TimTask selected; //Keep the instance of TimTask open in the editor here.
+    private static TimTask selected; //Keep the instance of TimTask opened in the editor here.
     private final VirtualFile delegate;
     private final ArrayList<String> headers;
     private final SubTask task;
     private static final HashMap<String, TimTask> CACHE = new HashMap<>();
-    //TODO: Add attributes for subtask info etc.
 
     /**
      * In order to "extend" VirtualFile we must have one as a delegate.
@@ -47,28 +46,23 @@ public final class TimTask {
      * @param project project that is open in the ide.
      */
     public void submit(Project project) {
-        //TODO: Implement submit here.
         Util.showWindow(project, "Output Window", true);
         try {
             CourseTaskPane.getInstance().setProgress(true, "Submitting...");
         } catch (Exception ex) {
-            LogHandler.logError("Submit action", ex);
+            LogHandler.logError("TimTask.submit(Project project)", ex);
         }
         this.syncChanges();
         new ApiHandler().submitExercise(this.delegate);
     }
 
     /**
-     * This method resets the exercise back to the state of latest submit.
+     * This method resets the exercise back to the state of the latest submit.
      */
     public void resetExercise() {
-        //TODO: Implement reset here.
-        ApiHandler handler = new ApiHandler();
-        ActiveState stateManager = ActiveState.getInstance();
-        String coursePath = stateManager.getCourseName(this.delegate.getPath());
         this.syncChanges();
         try {
-            handler.resetSubTask(this.delegate, coursePath);
+            new ApiHandler().resetSubTask(this.delegate, this.headers.getFirst());
         } catch (IOException ex) {
             com.api.LogHandler.logError("TimTask.resetExercise()", ex);
             InfoView.displayError(".timdata file not found!");
@@ -81,12 +75,11 @@ public final class TimTask {
     }
 
     /**
-     * This method is used to show the exercise in browser.
+     * This method is used to show the exercise's tim-page in browser.
      * @param baseURL URL for tim.
      * @param project project that is open in the ide.
      */
     public void openInBrowser(String baseURL, Project project) {
-        //TODO: Implement the action here.
         String url = baseURL;
         //TODO: handle null case.
         url += this.task.getPath();
@@ -107,10 +100,9 @@ public final class TimTask {
     }
 
     /**
-     * This method updates the changes in delegate VirtualFile to actual file on disk.
+     * This method updates the changes in the delegate VirtualFile to the physical file on disk.
      */
     private void syncChanges() {
-        //TODO: Implement updating the file on disk here.
         FileDocumentManager fileDocumentManager = FileDocumentManager.getInstance();
         Document document = fileDocumentManager.getDocument(this.delegate);
         if (document != null) {
@@ -119,10 +111,9 @@ public final class TimTask {
     }
 
     /**
-     * This method is used to update the information in the CourseTaskPane, and it's state etc.
+     * This method is used to update the SubTask information into CourseTaskPane, and to update it's state etc.
      */
     private static void messageUpdates() {
-        //TODO: Implement
         ActiveState.getInstance().setSubmittable(selected != null);
         ActiveState.getInstance().messageChanges();
     }
@@ -161,22 +152,20 @@ public final class TimTask {
 
     /**
      * This method is called from ActiveState when MessageBus event fires.
-     * @param file VirtualFile open in the editor which must be evaluated of being a tim task.
+     * @param file VirtualFile opened in the editor which must be evaluated.
      */
     public static void evaluateFile(VirtualFile file) {
-        //TODO: Make new TimTask if file open in the editor is one. Set selected null otherwise
-        //TODO: Maybe TimTasks once made could be cached for later use? Edit: Why not cache all results!?
         if (file != null && file.getCanonicalPath() != null) {
             if (CACHE.containsKey(file.getUrl())) {
                 selected = CACHE.get(file.getUrl());
             } else {
-                ArrayList<String> taskData = new ArrayList<>();
-                taskData.add(ActiveState.getInstance().getCourseName(file.getPath()));
-                taskData.add(ActiveState.getInstance().findTaskName(taskData.getFirst(), file));
+                ArrayList<String> taskHeaders = new ArrayList<>();
+                taskHeaders.add(ActiveState.getInstance().getCourseName(file.getPath()));
+                taskHeaders.add(ActiveState.getInstance().findTaskName(taskHeaders.getFirst(), file));
                 SubTask taskHolder = ActiveState.getInstance().findSubTask(file);
                 if (taskHolder != null) {
-                    taskData.add(taskHolder.getIdeTaskId());
-                    selected = new TimTask(file, taskData, taskHolder);
+                    taskHeaders.add(taskHolder.getIdeTaskId());
+                    selected = new TimTask(file, taskHeaders, taskHolder);
                     CACHE.put(file.getUrl(), selected);
                 } else {
                     selected = null;
@@ -186,7 +175,6 @@ public final class TimTask {
         } else {
             selected = null;
         }
-        //TODO: use ActiveState's setSubmittable to make actions enabled/disabled.
         messageUpdates();
     }
 }
