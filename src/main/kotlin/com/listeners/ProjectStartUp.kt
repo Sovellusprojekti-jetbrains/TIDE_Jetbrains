5b95c0d2188503c99f7bf99f49f452cda1c883fe
Lari Salminen <lari.j.a.salminen@student.jyu.fi>
2025-05-02 13:49:06 +0300

    Changed rules, started linting for real.

--- src/main/kotlin/com/listeners/ProjectStartUp.kt
+++ src/main/kotlin/com/listeners/ProjectStartUp.kt
@@ -9,21 +9,17 @@ import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.wm.ToolWindowManager
 import com.state.ActiveState
 import kotlinx.coroutines.runBlocking
-
-
 class ProjectStartUp : ProjectActivity {
-
-
     override suspend fun execute(project: Project) {
-        com.intellij.openapi.project.DumbService.getInstance(project).runWhenSmart { //Project must be ready first
+        com.intellij.openapi.project.DumbService.getInstance(project).runWhenSmart { // Project must be ready first
             // It seems that PersistentState is ready at this point
-            runBlocking { //Tries to prevent initialization of windows until login status is checked
+            runBlocking { // Tries to prevent initialization of windows until login status is checked
                 TideCommandExecutor.checkLogin()
             }
-            runBlocking { //This forces ActiveState instantiation I guess
+            runBlocking { // This forces ActiveState instantiation I guess
                 ActiveState.getInstance().initProjectDependents()
             }
-            runBlocking { //Not ideal but maybe better solution will be found
+            runBlocking { // Not ideal but maybe better solution will be found
                 while (true) {
                     val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("TIDE Tool Window")
                     if (toolWindow != null && (toolWindow.isAvailable)) {
@@ -32,19 +28,19 @@ class ProjectStartUp : ProjectActivity {
                 }
             }
             TideCommandExecutor.fetchCoursesAsync()
-            runBlocking { //This forces EditorFactory instantiation I guess
+            runBlocking { // This forces EditorFactory instantiation I guess
                 EditorFactory.getInstance()
             }
             runBlocking {
                 val fileEditorManager = FileEditorManager.getInstance(project)
                 val selectedEditor = fileEditorManager.selectedEditor
                 if (selectedEditor?.file != null) {
-                    //ActiveState.getInstance().setSubmittable(selectedEditor.file)
+                    // ActiveState.getInstance().setSubmittable(selectedEditor.file)
                     TimTask.evaluateFile(selectedEditor.file)
                 } else {
                     ActiveState.getInstance().messageChanges()
-                    //If CourseTaskPane is not open, messages might be ignored
-                    //Implementing CourseTaskPane differently with TimTask class will fix
+                    // If CourseTaskPane is not open, messages might be ignored
+                    // Implementing CourseTaskPane differently with TimTask class will fix
                 }
             }
         }
