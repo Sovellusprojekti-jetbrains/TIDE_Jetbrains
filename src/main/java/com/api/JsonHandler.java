e96b661cdd5af84c0c66dd382b76da21c342f714
Lari Salminen <rujasu92@yahoo.com>
2025-05-06 19:35:36 +0300

    Unused imports.

--- src/main/java/com/api/JsonHandler.java
+++ src/main/java/com/api/JsonHandler.java
@@ -4,8 +4,6 @@ import com.course.Course;
 import com.course.SubTask;
 import com.google.gson.*;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
