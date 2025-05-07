4774c31e912dfddf04ea2f6ee29b4cf0fbdf46ac
henrikoo2 <henri.k.karkkainen@jyu.fi>
2025-05-07 13:12:03 +0300

    Rajapinta käyttäjätilin hallintaan

--- src/main/java/com/interfaces/UserManagement.java
+++ src/main/java/com/interfaces/UserManagement.java
@@ -1,4 +1,31 @@
 package com.interfaces;
 
+/**
+ * This interface defines services for user's account management.
+ */
 public interface UserManagement {
+    /**
+     * This method is used to log user in into the TIM-system.
+     * TDE-CLI gets rights to the users account.
+     */
+    void login();
+
+    /**
+     * This method is used to log user out of the TIM-system.
+     * Rights to the users account are removed from TIDE-CLI.
+     */
+    void logout();
+
+    /**
+     *This method is used to access the list of user's courses in TIM-system.
+     */
+    void courses();
+
+    /**
+     * This method is used to download course tasks from user's TIM-courses.
+     * Downloaded tasks are appended into user metadata (.timdata file).
+     * @param courseDirectory Directory of the course task in TIM-system.
+     * @param cmdArgs Variable amount of arguments needed to use services offered by TIDE-CLI.
+     */
+    void loadExercise(String courseDirectory, String... cmdArgs);
 }
